6285735960eebecf2b14d8e157ddb0c2ec883364
unknown <pitaya1001@proton.me>
2023-06-03 09:51:21 -0300

    fix example

--- examples/client.py
+++ examples/client.py
@@ -2,14 +2,14 @@ import asyncio
 from sa import samp
 from sa.samp import RPC, MSG
 
-def on_message(message, internal_packet, peer):
+def on_message(message, internal_packet, peer, client):
     if message.id == MSG.RPC:
         rpc = message
         print(rpc)
 
 async def main():
     c = samp.Client(('127.0.0.1', 7777))
-    c.server_peer.connected_message_callbacks.append(on_message)
+    c.message_callbacks.append(on_message)
     await c.start()
     while True:
         await asyncio.sleep(0.01)
